7248d993b7095f1f1f404dea9c5d36a874a8d83e
Micha J Dev <vyxisprime@gmail.com>
2023-03-10 13:14:58 +0100

    Added WarnYmlHandler and Warn dto

--- src/nl/vBox/data/yml/handlers/Warns/WarnYmlHandler.java
+++ src/nl/vBox/data/yml/handlers/Warns/WarnYmlHandler.java
@@ -1,9 +1,62 @@
 package nl.vBox.data.yml.handlers.Warns;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
 import nl.vBox.Main;
+import nl.vBox.data.dataobjects.User;
+import nl.vBox.data.dataobjects.Warn;
+import nl.vBox.data.yml.handlers.YmlHandler;
+import nl.vBox.data.yml.handlers.Users.UserYmlHandler;
+import nl.vBox.utils.enums.LogSeverity;
 
 public class WarnYmlHandler {
 
 	private static Main main = Main.getInstance();
 
+	public static void createWarnsFolder(User u) {
+		File file = new File(UserYmlHandler.getUserFolder(u) + File.pathSeparator + "warns");
+		if (!file.exists()) {
+			main.log("Warns folder not found...Creating;", LogSeverity.INFO);
+			file.mkdirs();
+		}
+
+	}
+
+	public static File getWarnsFolder(User u) {
+		File file = new File(UserYmlHandler.getUserFolder(u) + File.pathSeparator + "warns");
+		if (!file.exists()) {
+			createWarnsFolder(u);
+			return file;
+		}
+		return file;
+	}
+
+	public static void createWarnsFile(Warn w) {
+		String hash = UUID.randomUUID().toString().substring(0, 5);
+		File file = new File(getWarnsFolder(w.getWarned()), "Warn#" + hash + ".yml");
+		
+		if (!file.exists()) {
+			try{
+				file.createNewFile();
+				FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+				createWarnLocals(w,cfg,file);
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	public static void createWarnLocals(Warn w, FileConfiguration cfg, File f) {
+		cfg.addDefault("Warner", w.getWarned().getUuid());
+		cfg.addDefault("Warned", w.getWarned().getUuid());
+		cfg.addDefault("Reason", w.getReason());
+		cfg.options().copyDefaults(true);
+		YmlHandler.saveConfig(cfg, f);
+	}
 }
